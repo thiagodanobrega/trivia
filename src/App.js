956d9fbc4862b297c2b1afb3025c64cb505f0251
jaogrs <jgabrielrodrigues2@gmail.com>
2022-03-10 10:32:41 -0300

    req-3-settings-screen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import ScreenGame from './pages/ScreenGame';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/game" component={ ScreenGame } />
+        <Route path="/settings" component={ Settings } />
       </Switch>
     </section>
   );
--- src/pages/ScreenGame.js
+++ src/pages/ScreenGame.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { getLocalStorage } from '../services/LocalStorage';
 
 class ScreenGame extends Component {
@@ -6,12 +7,28 @@ class ScreenGame extends Component {
     getLocalStorage('token');
   }
 
-  render() {
-    return (
-      <div>
-        <h1>Tela do jogo</h1>
-      </div>
-    );
-  }
+ onClickSettingsBtn = () => {
+   const { history } = this.props;
+   history.push('/settings');
+ }
+
+ render() {
+   return (
+     <div>
+       <h1>Tela do jogo</h1>
+       <button
+         type="button"
+         onClick={ this.onClickSettingsBtn }
+         data-testid="btn-settings"
+       >
+         Configurações
+       </button>
+     </div>
+   );
+ }
 }
+
+ScreenGame.propTypes = {
+  history: PropTypes.objectOf.isRequired,
+};
 export default ScreenGame;
