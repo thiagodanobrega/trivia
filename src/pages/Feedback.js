c40c2fa8533178f839c682dc90455b96a155b5a9
jaogrs <jgabrielrodrigues2@gmail.com>
2022-03-11 15:20:38 -0300

    req-13-assertions-text

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 export class Feedback extends Component {
+  renderFeedback = () => {
+    const THREE = 3;
+    const { assertions } = this.props;
+    if (assertions < THREE) {
+      return 'Could be better...';
+    }
+    return 'Well Done!';
+  }
+
   render() {
     return (
-      <div data-testid="feedback-text"><Header /></div>
+      <>
+        <Header />
+        <p data-testid="feedback-text">{this.renderFeedback()}</p>
+      </>
+
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
