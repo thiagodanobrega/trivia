e663117312cae78a52d057d9d0a189d15bd8a04d
Thiago Nobrega <thiagons@live.com>
2022-03-11 15:03:58 -0300

    req-15-play-again

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 export class Feedback extends Component {
+  redirectGame = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
-      <div data-testid="feedback-text"><Header /></div>
+      <div data-testid="feedback-text">
+        <Header />
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.redirectGame }
+        >
+          Play Again
+        </button>
+      </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.objectOf.isRequired,
+};
 
 export default Feedback;
