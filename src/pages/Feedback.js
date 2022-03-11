35492d38e08c433aaf991288b42749798c7982f3
Elimar Lucena <elimarlucenar@gmail.com>
2022-03-11 15:32:10 -0300

    req-14-numero-de-acertos-e-pontos

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 export class Feedback extends Component {
   render() {
+    const { player } = this.props;
     return (
-      <div data-testid="feedback-text"><Header /></div>
+      <div data-testid="feedback-text">
+        <Header />
+        <h3>
+          Você acertou
+          <p data-testid="feedback-total-score">{player.score}</p>
+          questões!
+        </h3>
+        <h3>
+          Um total de
+          <p data-testid="feedback-total-question">{player.assertions}</p>
+          pontos
+        </h3>
+      </div>
     );
   }
 }
+const { string } = PropTypes;
 
-export default Feedback;
+Feedback.propTypes = {
+  player: string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  player: state.player,
+});
+
+export default connect(mapStateToProps)(Feedback);
