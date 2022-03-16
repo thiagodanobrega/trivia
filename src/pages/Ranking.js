f8073016f7c5cb58cec2ff7e2fff1615ea7b2278
Elimar Lucena <elimarlucenar@gmail.com>
2022-03-16 13:12:17 -0300

    css ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { GiTrophy } from 'react-icons/gi';
 import { getLocalStorage } from '../services/LocalStorage';
+import '../style/ranking.css';
 
 class Ranking extends Component {
   state = {
@@ -23,35 +25,56 @@ class Ranking extends Component {
   render() {
     const { ranking } = this.state;
     return (
-      <div>
-        <h1 data-testid="ranking-title">Ranking</h1>
-        <button
-          type="button"
-          data-testid="btn-go-home"
-          onClick={ this.redirectLogin }
-        >
-          login
-        </button>
-        {
-          ranking.map((player, index) => (
-            <div key={ index }>
-              <p
-                data-testid={ `player-name-${index}` }
-              >
-                {player.name}
-              </p>
-              <p
-                data-testid={ `player-score-${index}` }
-              >
-                {player.score}
-              </p>
-              <img
-                src={ player.picture }
-                alt="imagem do jogador"
-              />
-            </div>
-          ))
-        }
+      <div className="main-ranking">
+        <div className="container-ranking">
+          <h1
+            className="title-ranking"
+            data-testid="ranking-title"
+          >
+            <GiTrophy className="trophy" />
+            LOADERBOARD
+          </h1>
+          {
+            ranking.map((player, index) => (
+              <div className="raking-container" key={ index }>
+                <div className={ `score style${index + 1}` }>{index + 1}</div>
+                <div
+                  className="player-ranking"
+                >
+                  <img
+                    className="picture-ranking"
+                    src={ player.picture }
+                    alt="imagem do jogador"
+                  />
+                  <div className="name-score-container">
+                    <p
+                      className="name-ranking"
+                      data-testid={ `player-name-${index}` }
+                    >
+                      {player.name}
+                    </p>
+                    <p
+                      className="score-ranking"
+                      data-testid={ `player-score-${index}` }
+                    >
+                      points:
+                      {' '}
+                      {player.score}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+          <button
+            type="button"
+            className="btn-ranking"
+            data-testid="btn-go-home"
+            onClick={ this.redirectLogin }
+          >
+            Login
+          </button>
+        </div>
       </div>
     );
   }
